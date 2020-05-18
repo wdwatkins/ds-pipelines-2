21f483b36743434e3a70cb1cbdaa73847c9a5dfb
Jordan S Read <jread@usgs.gov>
2020-05-18 05:28:07 -0500

    expose `units`

--- 3_visualize/src/plot_timeseries.R
+++ 3_visualize/src/plot_timeseries.R
@@ -1,6 +1,6 @@
-plot_nwis_timeseries <- function(filepath, site_data_styled, width = 12, height = 7){
+plot_nwis_timeseries <- function(filepath, site_data_styled, width = 12, height = 7, units = 'in'){
   
   ggplot(data = site_data_styled, aes(x = dateTime, y = water_temperature, color = station_name)) +
     geom_line() + theme_bw()
-  ggsave(filepath, width = width, height = height)
+  ggsave(filepath, width = width, height = height, units = units)
 }
