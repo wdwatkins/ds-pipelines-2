ea08b83489c6b827c225dcefbfb396005b43ab35
Jordan S Read <jread@usgs.gov>
2020-05-18 05:14:16 -0500

    whoops...

--- 3_visualize/src/plot_timeseries.R
+++ 3_visualize/src/plot_timeseries.R
@@ -2,5 +2,5 @@ plot_nwis_timeseries <- function(filepath, site_data_styled, width = 12, height 
   
   ggplot(data = site_data_styled, aes(x = dateTime, y = water_temperature, color = station_name)) +
     geom_line() + theme_bw()
-  ggsave(filepath, width = 12, height = 7)
+  ggsave(filepath, width = width, height = height)
 }
