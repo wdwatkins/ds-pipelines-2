600aa71694eeacb1ca04fee37b33f5d24d9276ed
Jordan S Read <jread@usgs.gov>
2020-05-23 15:50:29 -0500

    removing use of target_name, improving default args

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -1,16 +1,15 @@
 
-download_nwis_data <- function(site_nums = c("01427207", "01432160", "01435000", "01436690", "01466500", "01477050"), parameterCd = '00010'){
+download_nwis_data <- function(site_nums = c("01427207", "01432160", "01435000", "01436690", "01466500")){
   
   # create the file names that are needed for download_nwis_site_data
   # tempdir() creates a temporary directory that is wiped out when you start a new R session; 
   # replace tempdir() with "1_fetch/out" or another desired folder if you want to retain the download
   download_files <- file.path(tempdir(), paste0('nwis_', site_nums, '_data.csv'))
-  
   data_out <- data.frame(agency_cd = c(), site_no = c(), dateTime = c(), 
                          X_00010_00000 = c(), X_00010_00000_cd = c(), tz_cd = c())
   # loop through files to download 
   for (download_file in download_files){
-    download_nwis_site_data(download_file, parameterCd = parameterCd)
+    download_nwis_site_data(download_file, parameterCd = '00010')
     # read the downloaded data and append it to the existing data.frame
     these_data <- read_csv(download_file, col_types = 'ccTdcc')
     data_out <- rbind(data_out, these_data)
@@ -25,7 +24,7 @@ nwis_site_info <- function(fileout, site_data){
 }
 
 
-download_nwis_site_data <- function(filepath, parameterCd, startDate="2014-05-01", endDate="2015-05-01"){
+download_nwis_site_data <- function(filepath, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
   
   # filepaths look something like directory/nwis_01432160_data.csv,
   # remove the directory with basename() and extract the 01432160 with the regular expression match
