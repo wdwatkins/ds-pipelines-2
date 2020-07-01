a9b4d422865ae84cb9fce715d65993bf490f1dfb
wdwatkins <wwatkins@usgs.gov>
2020-07-01 13:52:01 -0700

    switch target types

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -1,17 +1,17 @@
-process_data <- function(all_site_files, site_filename) {
-  site_info <- read_csv(site_filename)
-  nwis_data <- do.call(bind_rows, lapply(all_site_files$filepath, read_csv))
+process_data <- function(file_out, all_site_files, site_info) {
+  all_site_files_df <- readRDS(all_site_files)
+  nwis_data <- do.call(bind_rows, lapply(all_site_files_df$filepath, read_csv))
   nwis_data_processed <- rename(nwis_data, water_temperature = X_00010_00000) %>% 
     select(-agency_cd, -X_00010_00000_cd, -tz_cd) %>% 
     left_join(site_info, by = "site_no") %>% 
     select(station_name = station_nm, site_no, dateTime, water_temperature, 
            latitude = dec_lat_va, longitude = dec_long_va) %>% 
     mutate(station_name = as.factor(station_name))
-  
-  return(nwis_data_processed)
+  saveRDS(nwis_data_processed, file = file_out)
 }
 
-hash_dir_files <- function(...) {
+hash_dir_files <- function(out_file, ...) {
   hash_vec <- tools::md5sum(c(...))
-  data.frame(filepath = names(hash_vec), hash = hash_vec)
+  hash_df <- data.frame(filepath = names(hash_vec), hash = hash_vec)
+  saveRDS(hash_df, file = out_file)
 }
--- 3_visualize/src/plot_timeseries.R
+++ 3_visualize/src/plot_timeseries.R
@@ -1,6 +1,6 @@
 plot_nwis_timeseries <- function(fileout, site_data_styled, width = 12, height = 7, units = 'in'){
-  
-  ggplot(data = site_data_styled, aes(x = dateTime, y = water_temperature, color = station_name)) +
+  site_data_df <- readRDS(site_data_styled)
+  ggplot(data = site_data_df, aes(x = dateTime, y = water_temperature, color = station_name)) +
     geom_line() + theme_bw()
   ggsave(fileout, width = width, height = height, units = units)
 }
